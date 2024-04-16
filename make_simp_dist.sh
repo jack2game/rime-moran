2722f3db4f4a607e9ba177f14c9eacf212295fc4
jack2game <rex.txt@gmail.com>
2024-04-16 12:06:20 +0200

    prep

--- make_simp_dist.sh
+++ make_simp_dist.sh
@@ -96,7 +96,7 @@ fi
 rm -rf dist/tools
 rm -rf dist/.git dist/.github dist/.gitignore
 rm -rf dist/make_simp_dist.sh
-cp 下载与安装说明.txt 更新纪要.txt dist
+# cp 下载与安装说明.txt 更新纪要.txt dist
 sedi 's/MORAN_VARIANT/简体/' dist/下载与安装说明.txt
 
 7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on "MoranSimplified-$(date +%Y%m%d).7z" dist
